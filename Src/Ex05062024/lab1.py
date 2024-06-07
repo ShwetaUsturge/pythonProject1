c50753a5a5ef8766b133041c5d181dcef31a3529
Shweta <ustureshweta@gmail.com>
2024-06-07 18:02:42 +0530

    pythonProject1.iml

---
+++ Src/Ex05062024/lab1.py
@@ -0,0 +1,17 @@
+print("Hi PyCharm")
+print("Shweta")
+age=30
+print(type(age))
+name= "Shweta"
+isMale= True
+pi=3.14
+print(type(name))
+print(type(isMale))
+print(type(pi))
+
+#complex numbers
+complesNumbers= 2+3j
+print(complesNumbers.imag)
+print(complesNumbers.real)
+
+
---
+++ Src/Ex05062024/lab2.py
@@ -0,0 +1,6 @@
+#take a input from user
+First_name= input("Enter Your First name")
+Last_name= input("Enter Your Last name")
+print("Your Name First name is:",First_name)
+print("Your Name Last name is:",Last_name)
+
